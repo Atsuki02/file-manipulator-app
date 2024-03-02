010d45d429c6d45f3c18e182953fad963a8a89b9
Atsuki Kitada <canvanvam@gmail.com>
2024-03-02 14:43:55 -0800

    add replace-string func

--- file_manipulator.py
+++ file_manipulator.py
@@ -3,6 +3,7 @@ import sys
 manipulator = sys.argv[1]
 input_path = sys.argv[2]
 output_path = sys.argv[3]
+newstring = sys.argv[4]
 
 
 def main(input_path, output_path, manipulator):
@@ -13,6 +14,8 @@ def main(input_path, output_path, manipulator):
         return copy(input_path, output_path)
     elif manipulator == 'duplicate-contents':
         return duplicate_contents(input_path, output_path)
+    elif manipulator == 'replace-string':
+        return replace_string(input_path, output_path, newstring)
     else:
         print(f"Unknown manipulator: {manipulator}")
 
@@ -43,6 +46,16 @@ def duplicate_contents(input_path, n):
     with open(input_path, 'w') as f:
         for _ in range(int(n) + 1):
             f.write(contents)
+
+def replace_string(input_path, needle, newstring):
+    # read input file
+    with open(input_path) as f:
+        contents = f.read()
+
+    # replace needle with newstring
+        replaced_string = contents.replace(needle, newstring)
+        with open(input_path, 'w') as f:
+            f.write(replaced_string)
 
         
 
