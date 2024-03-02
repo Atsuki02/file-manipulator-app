28977ab58e77169bccdb6d505ab0e1688f5cc062
Atsuki Kitada <canvanvam@gmail.com>
2024-03-02 14:29:53 -0800

    create copy and duplicate function

--- file_manipulator.py
+++ file_manipulator.py
@@ -5,9 +5,14 @@ input_path = sys.argv[2]
 output_path = sys.argv[3]
 
 
-def operate(input_path, output_path, manipulator):
+def main(input_path, output_path, manipulator):
     if manipulator == 'reverse':
         return reverse(input_path, output_path)
+    
+    elif manipulator == 'copy':
+        return copy(input_path, output_path)
+    elif manipulator == 'duplicate-contents':
+        return duplicate_contents(input_path, output_path)
     else:
         print(f"Unknown manipulator: {manipulator}")
 
@@ -21,5 +26,27 @@ def reverse(input_path, output_path):
     with open(output_path, 'w') as f:
         f.write(contents[::-1])
 
+def copy(input_path, output_path):
+    # read input file
+    with open(input_path) as f:
+        contents = f.read()
+
+    with open(output_path, 'w') as f:
+        f.write(contents)
+
+def duplicate_contents(input_path, n):
+    # read input file
+    with open(input_path) as f:
+        contents = f.read()
+
+    # duplicate input contents
+    with open(input_path, 'w') as f:
+        for _ in range(int(n) + 1):
+            f.write(contents)
+
+        
+
+
+
 # call function
-operate(input_path, output_path, manipulator)
+main(input_path, output_path, manipulator)
